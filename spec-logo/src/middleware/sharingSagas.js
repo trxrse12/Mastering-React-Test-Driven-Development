0338e7205702633d4887ce93f5f007f6e2cf8236
trxrse12 <trxrse@gmail.com>
2021-01-12 13:33:11 +0000

    The presenter is now able to stream events to the watcher

--- spec-logo/src/middleware/sharingSagas.js
+++ spec-logo/src/middleware/sharingSagas.js
@@ -1,4 +1,7 @@
 import { call, put, takeLatest, take } from 'redux-saga/effects';
+import {eventChannel, END} from 'redux-saga';
+
+let presenterSocket;
 
 // adapter layer between sagas (which work on generators)
 //    and WobSockets APIs (which work on callbacks passed by even
@@ -22,21 +25,70 @@ const buildUrl = (id) => {
 };
 
 function* startSharing() {
-  const presenterSocket = yield openWebSocket(); // this is the socket returned by the promise
+  presenterSocket = yield openWebSocket(); // this is the socket returned by the promise
   presenterSocket.send(JSON.stringify({type: 'START_SHARING'}));
   const message = yield receiveMessage(presenterSocket);
-  const presenterSessionId = JSON.parse(message).id; // eads the id from the server
+  const presenterSessionId = JSON.parse(message).id; // the id from the server
   yield put({
     type: 'STARTED_SHARING',
     url: buildUrl(presenterSessionId)
   })
 }
 
-function* stopSharing() {}
+function* stopSharing() {
+  presenterSocket.close();
+  yield put({type: 'STOPPED_SHARING'});
+}
 
-function* shareNewAction({ innerAction }) {}
+function* shareNewAction({ innerAction }) {
+  if (presenterSocket &&
+      presenterSocket.readyState === WebSocket.OPEN){
+    presenterSocket.send(
+      JSON.stringify({type: 'NEW_ACTION', innerAction})
+    )
+  }
+}
 
-function* startWatching() {}
+// used by the client to create an eventChannel at the client
+const webSocketListener = socket =>
+  eventChannel(emitter => {
+    socket.onmessage = emitter;
+    socket.onclose = () => emitter(END);
+    // now the unsubcribe function
+    return () => {
+      socket.onmessage = undefined;
+      socket.onclose = undefined;
+    }
+  });
+
+// used by the client side saga (see below) to watch the eventChannel
+function* watchUntilStopRequest(chan) {
+  try {
+    while (true) {
+      let evt = yield take(chan);
+      yield put(JSON.parse(evt.data));
+    }
+  } finally {
+    yield put({type: 'STOPPED_WATCHING'});
+  }
+}
+
+// the watcher side saga
+function* startWatching() {
+  const sessionId = new URLSearchParams(
+    window.location.search.substring(1)
+  ).get('watching');
+  if (sessionId){
+    const watcherSocket = yield openWebSocket();
+    yield put({type: 'RESET'});
+    watcherSocket.send(
+      JSON.stringify({type: 'START_WATCHING', id: sessionId})
+    );
+    yield put({type: 'STARTED_WATCHING'});
+    const channel = yield call(webSocketListener, watcherSocket);
+    yield call(watchUntilStopRequest, channel);
+  }
+}
 
 export function* sharingSaga() {
   yield takeLatest('TRY_START_WATCHING', startWatching);
--- spec-logo/test/middleware/sharingSagas.test.js
+++ spec-logo/test/middleware/sharingSagas.test.js
@@ -6,6 +6,9 @@ import {storeSpy, expectRedux} from 'expect-redux';
 import { act} from 'react-dom/test-utils';
 import {configureStore} from "../../src/store";
 
+const WEB_SOCKET_OPEN = WebSocket.OPEN;
+const WEB_SOCKET_CLOSED = WebSocket.CLOSED;
+
 describe('duplicateForSharing', () => {
   let dispatch;
   let store;
@@ -52,13 +55,50 @@ describe('sharing saga', () => {
   let socketSpyFactory;
   let sendSpy;
   let socketSpy;
+  let closeSpy;
+
+  // test helper that triggers the websockets's message event manually for the TDD purpose
+  const notifySocketOpened = async () => {
+    await act(async () => {
+      socketSpy.onopen()
+    })
+  };
+
+  // test helper that triggers the websocket's onmessage event manually for TDD purpose
+  const sendSocketMessage = async message => {
+    await act(async () => {
+      socketSpy.onmessage({data: JSON.stringify(message) })
+    });
+  };
+
+  const startSharing = async sessionid => {
+    store.dispatch({type: 'START_SHARING'});
+    await notifySocketOpened();
+    await sendSocketMessage({type: 'UNKNOWN', id: 123});
+  };
+
+  const startWatching = async () => {
+    await act(async () => {
+      store.dispatch({type: 'TRY_START_WATCHING'});
+      socketSpy.onopen();
+    })
+  }
 
   beforeEach(() => {
     sendSpy = jest.fn();
+    closeSpy = jest.fn();
     socketSpyFactory = jest.spyOn(window, 'WebSocket');
+    Object.defineProperty(socketSpyFactory, 'OPEN', {
+      value: WEB_SOCKET_OPEN
+    });
+    Object.defineProperty(socketSpyFactory, 'CLOSED', {
+      value: WEB_SOCKET_CLOSED
+    });
     socketSpyFactory.mockImplementation(() => {
       socketSpy = {
-        send: sendSpy
+        send: sendSpy,
+        close: closeSpy,
+        readyState: WebSocket.OPEN,
       };
       return socketSpy;
     });
@@ -78,21 +118,6 @@ describe('sharing saga', () => {
   });
 
   describe('START_SHARING', () => {
-
-    // test helper that triggers the websockets's message event manually for the TDD purpose
-    const notifySocketOpened = async () => {
-      await act(async () => {
-        socketSpy.onopen()
-      })
-    };
-
-    // test helper that triggers the websocket's onmessage event manually for TDD purpose
-    const sendSocketMessage = async message => {
-      await act(async () => {
-        socketSpy.onmessage({data: JSON.stringify(message) })
-      });
-    };
-
     it('opens a websocket when starting to share', () => {
       store.dispatch({type: 'START_SHARING'});
       expect(socketSpyFactory).toHaveBeenCalledWith('ws://test:1234/share');
@@ -116,6 +141,124 @@ describe('sharing saga', () => {
           type: 'STARTED_SHARING',
           url: 'http://test:1234/index.html?watching=123',
         })
+    });
+  });
+
+  describe('START_SHARING', () => {
+    it('calls close on the open socket', async () => {
+      await startSharing();
+      store.dispatch({type: 'STOP_SHARING'});
+      expect(closeSpy).toHaveBeenCalled();
+    });
+
+    it('dispatches an action of STOPPED_SHARING',async () => {
+      await startSharing();
+      store.dispatch({type: 'STOP_SHARING'});
+      return expectRedux(store)
+        .toDispatchAnAction()
+        .matching({type: 'STOPPED_SHARING'});
+    });
+  });
+
+  describe('SHARE_NEW_ACTION', () => {
+    it('forwards the same action on the socket',async () => {
+      const innerAction = {a:123};
+      await startSharing(123);
+      store.dispatch({type: 'SHARE_NEW_ACTION', innerAction});
+      expect(sendSpy).toHaveBeenLastCalledWith(
+        JSON.stringify({type: 'NEW_ACTION', innerAction})
+      )
+    });
+
+    it('does NOT forward if the socket is not set yet', () => {
+      store.dispatch({type: 'SHARE_NEW_ACTION'});
+      expect(sendSpy).not.toHaveBeenCalled();
+    });
+
+    it('does NOT forward if the socket has been closed',async () => {
+      await startSharing(123);
+      socketSpy.readyState = WebSocket.CLOSED;
+      store.dispatch({type: 'SHARE_NEW_ACTION'});
+      expect(sendSpy.mock.calls.length).toEqual(1);
+    });
+  });
+
+  describe('watching', () => {
+    beforeEach(() => {
+      Object.defineProperty(window, 'location', {
+        writable: true,
+        value: {
+          host: 'test:1234',
+          pathname: './index.html',
+          search: '?watching=234',
+        }
+      });
+    });
+
+    it('opens a socket when the page loads', () => {
+      store.dispatch({type: 'TRY_START_WATCHING'});
+      expect(socketSpyFactory).toHaveBeenCalledWith(
+        'ws://test:1234/share' // it comes from the mock above
+      )
+    });
+
+    it('does NOT open socket if the watching field is not set', () => {
+      window.location.search = '?';
+      store.dispatch({type: 'TRY_START_WATCHING'});
+      expect(socketSpyFactory).not.toHaveBeenCalled();
+    });
+
+    it('dispatches a RESET action',async () => {
+      await startWatching();
+      return expectRedux(store)
+        .toDispatchAnAction()
+        .matching({type: 'RESET'});
+    });
+
+    it('sends the session id to the socket with an action type of START_WATCHING',async () => {
+      await startWatching();
+      expect(sendSpy).toHaveBeenCalledWith(
+        JSON.stringify({type:'START_WATCHING', id: '234'})
+      )
+    });
+
+    it('dispatches a STARTING_WATCHING action', async () => {
+      await startWatching();
+      return expectRedux(store)
+        .toDispatchAnAction()
+        .matching({type: 'STARTED_WATCHING'})
+    });
+
+    it('relays multiple actions from the websocket',async () => {
+      const message1 = {type: 'ABC'};
+      const message2 = {type: 'BCD'};
+      const message3 = {type: 'CDE'};
+      await startWatching();
+      await sendSocketMessage(message1);
+      await sendSocketMessage(message2);
+      await sendSocketMessage(message3);
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .matching(message1);
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .matching(message2);
+
+      await expectRedux(store)
+        .toDispatchAnAction()
+        .matching(message3);
+
+      socketSpy.onclose();
+    });
+
+    it('sends a STOPPED_WATCHING action when the connection is closed', async () => {
+      await startWatching();
+      socketSpy.onclose();
+      return expectRedux(store)
+        .toDispatchAnAction()
+        .matching({type: 'STOPPED_WATCHING'});
     });
   });
 });
